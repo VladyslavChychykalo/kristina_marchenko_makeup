d4e94a678fc154721bbdf194d8ca69522df74e63
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-03-04 21:53:03 +0200

    mod

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,41 +1,110 @@
+// import React, { Component } from "react";
+// import { homeWrapper } from "./Home.module.css";
+// import gallery from "./gallery/gallery";
+// import { CSSTransition, TransitionGroup } from "react-transition-group";
+// import slideRandomTransaction from "../../stylesheet/transitions/slideRandomImage.module.css";
+
+// export default class Home extends Component {
+//   state = {
+//     image: "",
+//     didMount: false
+//   };
+
+//   componentDidMount() {
+//     this.updateImage();
+//     this.interval = setInterval(this.updateImage, 5000);
+//     console.log(this.interval);
+//     console.log("cdm");
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.didMount === prevState.didMount) {
+//       this.setState(state => ({
+//         didMount: !state.didMount
+//       }));
+
+//       return;
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.interval);
+//   }
+
+//   randomImage = image => {
+//     this.setState({
+//       image: image
+//     });
+//   };
+
+//   updateImage = () => {
+//     const randomImg = gallery[Math.floor(Math.random() * gallery.length)];
+//     console.log(gallery[0]);
+//     this.randomImage(randomImg);
+//   };
+
+//   render() {
+//     const { image, didMount } = this.state;
+//     console.log(this.state);
+//     console.log(image);
+//     return (
+//       <CSSTransition
+//         // in={didMount}
+//         timeout={2500}
+//         classNames={slideRandomTransaction}
+//       >
+//         <div className={homeWrapper}>
+//           <img src={image} alt="random_image" />
+//         </div>
+//       </CSSTransition>
+//     );
+//   }
+// }
+
 import React, { Component } from "react";
 import { homeWrapper } from "./Home.module.css";
 import gallery from "./gallery/gallery";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import slideRandomTransaction from "../../stylesheet/transitions/slideRandomImage.module.css";
 
 export default class Home extends Component {
-  state = {
-    image: ""
-  };
+  state = { index: -1, didMount: true };
 
   componentDidMount() {
-    this.updateImage();
-    this.interval = setInterval(this.updateImage, 5000);
+    this.nextImage();
+    this.interval = setInterval(this.nextImage, 4000);
     console.log(this.interval);
+    console.log("cdm");
   }
 
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
-  randomImage = image => {
-    this.setState({
-      image: image
-    });
-  };
+  nextImage = () => {
+    console.log("func");
+    const { index } = this.state;
 
-  updateImage = () => {
-    const randomImg = gallery[Math.floor(Math.random() * gallery.length)];
-    this.randomImage(randomImg);
+    if (gallery.length - 1 === index) {
+      this.setState({
+        index: -1
+      });
+    }
+
+    this.setState(state => ({
+      index: state.index + 1
+    }));
   };
 
   render() {
-    const { image } = this.state;
-    console.log(this.state);
-    console.log(image);
+    const { index } = this.state;
+    console.log(index);
     return (
-      <div className={homeWrapper}>
-        <img src={image} alt="random_image" />
-      </div>
+      <CSSTransition timeout={1000} classNames={slideRandomTransaction}>
+        <div className={homeWrapper}>
+          <img src={gallery[index]} alt="random_image" />
+        </div>
+      </CSSTransition>
     );
   }
 }
