ac1c1fcad7a3a6ff917b8be4ef4f0b3f8124a388
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-03-05 22:59:51 +0200

    mod

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Map from "./Map/Map";
-import { info, contactsWrapper, link } from "./Contacts.module.css";
+import WrappedMap from "./Map/Map";
+import { map, info, contactsWrapper, link } from "./Contacts.module.css";
 import { ReactComponent as Instagram } from "../../assets/svg/instagramIcon.svg";
 import { ReactComponent as Facebook } from "../../assets/svg/facebookIcon.svg";
 
@@ -22,8 +22,15 @@ const Contacts = () => (
         <Facebook width="35px" height="35px" />
       </a>
     </div>
-    <div className="map">
-      <Map />
+    <div className={map}>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCVwLxr93PoHBzTHHtifMaDRfVMME7y0eo&v=3.exp&libraries=geometry,drawing,places`}
+        // googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCVwLxr93PoHBzTHHtifMaDRfVMME7y0eo&v=3.exp&libraries=geometry,drawing,places`}
+        // googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   </div>
 );
--- src/components/Contacts/Map/Map.js
+++ src/components/Contacts/Map/Map.js
@@ -1,5 +1,20 @@
 import React from "react";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker
+} from "react-google-maps";
 
-const Map = () => <div></div>;
+const Map = () => (
+  <GoogleMap
+    defaultZoom={15}
+    defaultCenter={{ lat: 50.424655, lng: 30.534771 }}
+  >
+    <Marker position={{ lat: 50.424655, lng: 30.534771 }} />
+  </GoogleMap>
+);
 
-export default Map;
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default WrappedMap;
