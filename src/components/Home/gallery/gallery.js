d0ea2c2338e17305ec547c166b628cb3a4ea80f9
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-03-06 23:37:47 +0200

    mod

--- src/components/Home/gallery/gallery.js
+++ src/components/Home/gallery/gallery.js
@@ -1,87 +1,17 @@
-import portfolioImage1 from "../../../assets/images/portfolio/portfolioImage1.JPG";
-import portfolioImage2 from "../../../assets/images/portfolio/portfolioImage2.JPG";
-import portfolioImage3 from "../../../assets/images/portfolio/portfolioImage3.JPG";
-import portfolioImage4 from "../../../assets/images/portfolio/portfolioImage4.JPG";
-import portfolioImage5 from "../../../assets/images/portfolio/portfolioImage5.JPG";
-import portfolioImage6 from "../../../assets/images/portfolio/portfolioImage6.JPG";
-import portfolioImage7 from "../../../assets/images/portfolio/portfolioImage7.JPG";
-import portfolioImage8 from "../../../assets/images/portfolio/portfolioImage8.JPG";
-import portfolioImage9 from "../../../assets/images/portfolio/portfolioImage9.JPG";
-import portfolioImage10 from "../../../assets/images/portfolio/portfolioImage10.JPG";
-import portfolioImage11 from "../../../assets/images/portfolio/portfolioImage11.JPG";
-import portfolioImage12 from "../../../assets/images/portfolio/portfolioImage12.JPG";
-import portfolioImage13 from "../../../assets/images/portfolio/portfolioImage13.JPG";
-import portfolioImage14 from "../../../assets/images/portfolio/portfolioImage14.JPG";
-import portfolioImage15 from "../../../assets/images/portfolio/portfolioImage15.JPG";
-import portfolioImage16 from "../../../assets/images/portfolio/portfolioImage16.JPG";
-import portfolioImage17 from "../../../assets/images/portfolio/portfolioImage17.JPG";
-import portfolioImage18 from "../../../assets/images/portfolio/portfolioImage18.JPG";
-import portfolioImage19 from "../../../assets/images/portfolio/portfolioImage19.JPG";
-import portfolioImage20 from "../../../assets/images/portfolio/portfolioImage20.JPG";
-import portfolioImage21 from "../../../assets/images/portfolio/portfolioImage21.JPG";
-import portfolioImage22 from "../../../assets/images/portfolio/portfolioImage22.JPG";
-import portfolioImage23 from "../../../assets/images/portfolio/portfolioImage23.JPG";
-// import portfolioImage24 from "../../../assets/images/portfolio/portfolioImage24.JPG";
-import portfolioImage25 from "../../../assets/images/portfolio/portfolioImage25.JPG";
-import portfolioImage26 from "../../../assets/images/portfolio/portfolioImage26.JPG";
-import portfolioImage27 from "../../../assets/images/portfolio/portfolioImage27.JPG";
-import portfolioImage28 from "../../../assets/images/portfolio/portfolioImage28.JPG";
-import portfolioImage29 from "../../../assets/images/portfolio/portfolioImage29.JPG";
-import portfolioImage30 from "../../../assets/images/portfolio/portfolioImage30.JPG";
-import portfolioImage31 from "../../../assets/images/portfolio/portfolioImage31.JPG";
-import portfolioImage32 from "../../../assets/images/portfolio/portfolioImage32.JPG";
-import portfolioImage33 from "../../../assets/images/portfolio/portfolioImage33.JPG";
-import portfolioImage34 from "../../../assets/images/portfolio/portfolioImage34.JPG";
-import portfolioImage35 from "../../../assets/images/portfolio/portfolioImage35.JPG";
-import portfolioImage36 from "../../../assets/images/portfolio/portfolioImage36.JPG";
-import portfolioImage37 from "../../../assets/images/portfolio/portfolioImage37.JPG";
-import portfolioImage38 from "../../../assets/images/portfolio/portfolioImage38.JPG";
-import portfolioImage39 from "../../../assets/images/portfolio/portfolioImage39.JPG";
-import portfolioImage40 from "../../../assets/images/portfolio/portfolioImage40.JPG";
-import portfolioImage41 from "../../../assets/images/portfolio/portfolioImage41.JPG";
+import homeImage1 from "../../../assets/images/portfolio/portfolioImage24.JPG";
+import homeImage2 from "../../../assets/images/portfolio/portfolioImage15.JPG";
+import homeImage3 from "../../../assets/images/portfolio/portfolioImage1.JPG";
+import homeImage4 from "../../../assets/images/courses/makeupPhoto4.JPG";
+import homeImage5 from "../../../assets/images/portfolio/portfolioImage14.JPG";
+import homeImage6 from "../../../assets/images/portfolio/portfolioImage28.JPG";
 
 const gallery = [
-  portfolioImage1,
-  portfolioImage2,
-  portfolioImage3,
-  portfolioImage4,
-  portfolioImage5,
-  portfolioImage6,
-  portfolioImage7,
-  portfolioImage8,
-  portfolioImage9,
-  portfolioImage10,
-  portfolioImage11,
-  portfolioImage12,
-  portfolioImage13,
-  portfolioImage14,
-  portfolioImage15,
-  portfolioImage16,
-  portfolioImage17,
-  portfolioImage18,
-  portfolioImage19,
-  portfolioImage20,
-  portfolioImage21,
-  portfolioImage22,
-  portfolioImage23,
-  // portfolioImage24,
-  portfolioImage25,
-  portfolioImage26,
-  portfolioImage27,
-  portfolioImage28,
-  portfolioImage29,
-  portfolioImage30,
-  portfolioImage31,
-  portfolioImage32,
-  portfolioImage33,
-  portfolioImage34,
-  portfolioImage35,
-  portfolioImage36,
-  portfolioImage37,
-  portfolioImage38,
-  portfolioImage39,
-  portfolioImage40,
-  portfolioImage41
+  homeImage1,
+  homeImage2,
+  homeImage3,
+  homeImage4,
+  homeImage5,
+  homeImage6
 ];
 
 export default gallery;
