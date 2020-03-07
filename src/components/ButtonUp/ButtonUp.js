0fd0d6db2590b85ec387d4b2c352598995b0ba2d
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-03-07 22:11:29 +0200

    added buttonup ui

---
+++ src/components/ButtonUp/ButtonUp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { buttonUpWrapper } from "./ButtonUp.module.css";
+
+const ButtonUp = () => (
+  <div className={buttonUpWrapper}>
+    <p>&#8679;</p>
+  </div>
+);
+
+export default ButtonUp;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import { CSSTransition } from "react-transition-group";
+import ButtonUp from "../ButtonUp/ButtonUp";
 import gallery from "./gallery/gallery";
 import { portfolioWrapper } from "./Portfolio.module.css";
+import fadeButtonUpTransition from "../../stylesheet/transitions/fadeButtonUp.module.css";
 
-const Portfolio = () => (
-  <ul className={portfolioWrapper}>
-    {gallery.map(el => (
-      <li>
-        <img src={el} alt="image_makeup" />
-      </li>
-    ))}
-  </ul>
-);
+export default class Portfolio extends Component {
+  state = { isAppear: false };
 
-export default Portfolio;
+  componentDidMount() {
+    window.addEventListener("scroll", this.listenScrollEvent);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.listenScrollEvent);
+  }
+
+  listenScrollEvent = e => {
+    if (window.scrollY > 1500) {
+      this.setState({ isAppear: true });
+    } else {
+      this.setState({ isAppear: false });
+    }
+  };
+
+  render() {
+    const { isAppear } = this.state;
+    return (
+      <>
+        <ul className={portfolioWrapper}>
+          {gallery.map(el => (
+            <li>
+              <img src={el} alt="image_makeup" />
+            </li>
+          ))}
+        </ul>
+        <CSSTransition
+          in={isAppear}
+          timeout={1000}
+          classNames={fadeButtonUpTransition}
+          unmountOnExit
+        >
+          <ButtonUp />
+        </CSSTransition>
+      </>
+    );
+  }
+}
