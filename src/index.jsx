4ffb06f9c1048d249edca1b5086e300403762eab
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-03-10 23:55:34 +0200

    mod

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import App from "./components/App";
 import { HashRouter, Route } from "react-router-dom";
 import "./stylesheet/main.css";
 
+window.screen.orientation.lock("any");
+
 ReactDOM.render(
   <HashRouter basename="/">
     <Route component={App} />
