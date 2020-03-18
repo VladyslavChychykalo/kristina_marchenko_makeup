77aca461ad0b8d3b4330be437d0a1ae657040b81
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-03-18 21:57:01 +0200

    mod

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,11 @@
-import React, { Component, Suspense, lazy } from "react";
+import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-// import Loader from "../components/Loader/Loader";
 import Header from "./Header/Header";
 import ToggleMenu from "./ToggleMenu/ToggleMenu";
 import routes from "../routes/routes";
 import slideMenuTransition from "../stylesheet/transitions/slideMenu.module.css";
 import { CSSTransition } from "react-transition-group";
 import { container } from "./App.module.css";
-
-const AsyncCoursesPage = lazy(() =>
-  import(
-    "../pages/CoursesPage/CoursesPage" /* webpackChunkName: "courses-page" */
-  )
-);
 
 export default class App extends Component {
   state = { isOpen: false };
@@ -36,54 +29,52 @@ export default class App extends Component {
         >
           <Header onToggle={this.toggleMenu} />
         </CSSTransition>
-        <Suspense fallback={<h1>Loading...</h1>}>
-          <div className={container}>
-            <ToggleMenu onToggle={this.toggleMenu} />
-            <Switch>
-              <Route
-                path={routes.HOME_PAGE.path}
-                component={routes.HOME_PAGE.component}
-              />
-              <Route
-                path={routes.PORTFOLIO_PAGE.path}
-                component={routes.PORTFOLIO_PAGE.component}
-              />
-              <Route
-                path={routes.SERVICES_PAGE.path}
-                component={routes.SERVICES_PAGE.component}
-              />
-              <Route
-                path={routes.HAIRSTYLE_COURSE_PAGE.path}
-                component={routes.HAIRSTYLE_COURSE_PAGE.component}
-              />
-              <Route
-                path={routes.MAKEUP_FOR_YOUSELF_PAGE.path}
-                component={routes.MAKEUP_FOR_YOUSELF_PAGE.component}
-              />
-              <Route
-                path={routes.REFRESHER_COURSE_PAGE.path}
-                component={routes.REFRESHER_COURSE_PAGE.component}
-              />
-              <Route
-                path={routes.EXPRESS_MAKEUP_PAGE.path}
-                component={routes.EXPRESS_MAKEUP_PAGE.component}
-              />
-              <Route
-                path={routes.BASIC_COURSE_PAGE.path}
-                component={routes.BASIC_COURSE_PAGE.component}
-              />
-              <Route
-                path={routes.COURSES_PAGE.path}
-                component={AsyncCoursesPage}
-              />
-              <Route
-                path={routes.CONTACTS_PAGE.path}
-                component={routes.CONTACTS_PAGE.component}
-              />
-              <Redirect to={routes.HOME_PAGE.path} />
-            </Switch>
-          </div>
-        </Suspense>
+        <div className={container}>
+          <ToggleMenu onToggle={this.toggleMenu} />
+          <Switch>
+            <Route
+              path={routes.HOME_PAGE.path}
+              component={routes.HOME_PAGE.component}
+            />
+            <Route
+              path={routes.PORTFOLIO_PAGE.path}
+              component={routes.PORTFOLIO_PAGE.component}
+            />
+            <Route
+              path={routes.SERVICES_PAGE.path}
+              component={routes.SERVICES_PAGE.component}
+            />
+            <Route
+              path={routes.HAIRSTYLE_COURSE_PAGE.path}
+              component={routes.HAIRSTYLE_COURSE_PAGE.component}
+            />
+            <Route
+              path={routes.MAKEUP_FOR_YOUSELF_PAGE.path}
+              component={routes.MAKEUP_FOR_YOUSELF_PAGE.component}
+            />
+            <Route
+              path={routes.REFRESHER_COURSE_PAGE.path}
+              component={routes.REFRESHER_COURSE_PAGE.component}
+            />
+            <Route
+              path={routes.EXPRESS_MAKEUP_PAGE.path}
+              component={routes.EXPRESS_MAKEUP_PAGE.component}
+            />
+            <Route
+              path={routes.BASIC_COURSE_PAGE.path}
+              component={routes.BASIC_COURSE_PAGE.component}
+            />
+            <Route
+              path={routes.COURSES_PAGE.path}
+              component={routes.COURSES_PAGE.component}
+            />
+            <Route
+              path={routes.CONTACTS_PAGE.path}
+              component={routes.CONTACTS_PAGE.component}
+            />
+            <Redirect to={routes.HOME_PAGE.path} />
+          </Switch>
+        </div>
       </>
     );
   }
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Loader = () => <div></div>;
+const Loader = () => <div>Loading...</div>;
 
 export default Loader;
