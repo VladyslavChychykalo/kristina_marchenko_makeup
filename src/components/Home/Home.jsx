db3d952632fb6a0bd079ac5b885d6cdb2369ad72
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-03-19 00:03:01 +0200

    mod special home height

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -32,6 +32,9 @@ export default class Home extends Component {
 
   render() {
     const { index } = this.state;
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
     return (
       <>
         <div className={backGround}></div>
