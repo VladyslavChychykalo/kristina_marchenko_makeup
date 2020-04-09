b5a8b59ae73db6eadc427b768301c8b2b17ff0c9
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-04-09 17:32:21 +0300

    mod

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import Media from "react-media";
 import DesktopMenu from "./DesktopMenu/DesktopMenu";
 import ToggleMenu from "./ToggleMenu/ToggleMenu";
 
 const Menu = ({ onToggle }) => {
-  const windowWidth = document.documentElement.clientWidth;
+  return (
+    <div>
+      <Media queries={{ small: { minWidth: 1199 } }}>
+        {(matches) =>
+          matches.small ? <DesktopMenu /> : <ToggleMenu onToggle={onToggle} />
+        }
+      </Media>
+    </div>
+  );
+  // const windowWidth = document.documentElement.clientWidth;
 
-  if (windowWidth >= 1200) {
-    return <DesktopMenu />;
-  }
+  // if (windowWidth >= 1200) {
+  //   return <DesktopMenu />;
+  // }
 
-  return <ToggleMenu onToggle={onToggle} />;
+  // return <ToggleMenu onToggle={onToggle} />;
 };
 
 export default Menu;
