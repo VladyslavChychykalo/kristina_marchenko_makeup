fc8c3d091276be2e8d6adc00ddd10e5c140f27b7
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2020-04-13 17:03:19 +0300

    mod

--- src/components/Menu/ToggleMenu/ToggleMenu.jsx
+++ src/components/Menu/ToggleMenu/ToggleMenu.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { buttonToggle, logoImg } from "./ToggleMenu.module.css";
-// import Logo from "../../Logo/Logo";
 import logo from "../../../assets/images/logo.gif";
 
 const ToggleMenu = ({ onToggle }) => (
   <div className={buttonToggle}>
     <button onClick={onToggle}>Меню</button>
     <img className={logoImg} src={logo} alt="" />
-    {/* <Logo /> */}
   </div>
 );
 
